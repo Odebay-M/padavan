a3b8b62001825f8097ff32def51896c5fe77d389
Odebay-M <aliababve@gmail.com>
2026-04-15 22:46:05 +0700

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
 
-# 1. Переходим в папку с компонентом zapret (в padavan-ng он в user/nfqws)
+# 1. Заходим в папку nfqws внутри дерева исходников
+# В вашем воркфлоу папка называется padavan-ng
 cd padavan-ng/trunk/user/nfqws
 
-# 2. Удаляем старые исходники
+# 2. Очищаем всё старое
 rm -rf *
 
-# 3. Клонируем zapret2 напрямую в эту папку
-git clone --depth=1 https://github.com/bol-van/zapret2
+# 3. Клонируем zapret2 правильно (с точкой в конце)
+git clone --depth=1 https://github.com/bol-van/zapret2 .
 
-# 4. Создаем новый Makefile специально для Padavan, чтобы он собрал nfqws2
+# 4. Создаем Makefile (исправлены пути к объектным файлам)
 cat << 'EOF' > Makefile
-# Простой Makefile для сборки nfqws2 в окружении Padavan
-TGT := nfqws2
-SRCDIR := nfqws
-SRC := $(SRCDIR)/nfqws.c $(SRCDIR)/helpers.c $(SRCDIR)/sec.c $(SRCDIR)/conntrack.c $(SRCDIR)/protocol.c $(SRCDIR)/params.c $(SRCDIR)/packets.c $(SRCDIR)/desync.c $(SRCDIR)/hostlist.c $(SRCDIR)/proxy.c
+TGT1 := nfqws2
+SRC1 := nfqws/nfqws.c nfqws/helpers.c nfqws/sec.c nfqws/conntrack.c nfqws/protocol.c nfqws/params.c nfqws/packets.c nfqws/desync.c nfqws/hostlist.c nfqws/proxy.c
 
-all: $(TGT)
+TGT2 := tpws
+SRC2 := tpws/tpws.c tpws/helpers.c tpws/params.c tpws/protocol.c tpws/sec.c tpws/hostlist.c
 
-$(TGT): $(SRC)
-	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $^ -lnetfilter_queue -lnfnetlink -lpthread
+all: $(TGT1) $(TGT2)
+
+$(TGT1): $(SRC1)
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $^ -lnetfilter_queue -lnfnetlink -lpthread -lz
+
+$(TGT2): $(SRC2)
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $^ -lpthread -lz
 
 clean:
-	rm -f $(TGT)
+	rm -f $(TGT1) $(TGT2)
 
 romfs:
-	$(ROMFSINST) /usr/bin/$(TGT)
+	$(ROMFSINST) /usr/bin/$(TGT1)
+	$(ROMFSINST) /usr/bin/$(TGT2)
 EOF
 
-# 5. (Опционально) Исправляем скрипты запуска прошивки, чтобы они искали nfqws2 вместо nfqws
+# 5. Возвращаемся в корень репозитория padavan-ng
 cd ../../../
+
+# 6. Включаем необходимые опции в .config
+# Используем седы для поиска и замены, если опция уже есть
+sed -i 's/^# CONFIG_FIRMWARE_INCLUDE_NFQWS is not set/CONFIG_FIRMWARE_INCLUDE_NFQWS=y/' trunk/.config
+grep -q "CONFIG_FIRMWARE_INCLUDE_LIBNETFILTER_QUEUE=y" trunk/.config || echo "CONFIG_FIRMWARE_INCLUDE_LIBNETFILTER_QUEUE=y" >> trunk/.config
+grep -q "CONFIG_FIRMWARE_INCLUDE_ZLIB=y" trunk/.config || echo "CONFIG_FIRMWARE_INCLUDE_ZLIB=y" >> trunk/.config
+
+# 7. Глобальная замена вызова бинарника в скриптах прошивки
 find trunk/user/scripts -type f -name "*.sh" -exec sed -i 's/nfqws/nfqws2/g' {} +
+
+echo "Pre-build stage for zapret2 finished successfully."
