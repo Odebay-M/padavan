62aedbe7359dc37da0e7cae244a8af7f134d0806
Odebay-M <aliababve@gmail.com>
2026-04-15 13:36:25 +0700

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-
-# Переходим в директорию, куда скачиваются исходники
 cd padavan-ng/trunk
 
-# Удаляем старый бинарник, если он там есть, и качаем zapret2 (nfqws)
-# Для Mi Router 3 (MT7620) строго mips32r1-softfloat
+# 1. Принудительно создаем папку для nfqws
 mkdir -p user/nfqws
-curl -L -o user/nfqws/nfqws https://github.com
 
-# Даем права на выполнение
+# 2. Скачиваем бинарник nfqws (Zapret 2) для MT7620
+# Мы берем его из основной ветки, где лежит актуальная 2-я версия
+curl -L -o user/nfqws/nfqws https://github.com
 chmod +x user/nfqws/nfqws
 
-# Отключаем тяжелые пакеты прямо здесь (на случай если забыли в build.config),
-# чтобы прошивка точно влезла в 14МБ
-sed -i 's/CONFIG_FIRMWARE_INCLUDE_ARIA=y/CONFIG_FIRMWARE_INCLUDE_ARIA=n/' .config
-sed -i 's/CONFIG_FIRMWARE_INCLUDE_TRANSMISSION=y/CONFIG_FIRMWARE_INCLUDE_TRANSMISSION=n/' .config
-sed -i 's/CONFIG_FIRMWARE_INCLUDE_TOR=y/CONFIG_FIRMWARE_INCLUDE_TOR=n/' .config
+# 3. ВАЖНО: Модифицируем Makefile nfqws, чтобы он НЕ скачивал старье
+# и НЕ пытался ничего компилировать, а просто взял наш готовый файл
+cat <<EOF > user/nfqws/Makefile
+all:
+clean:
+romfs:
+	\$(ROMFSINST) -p +x \$(ROOTDIR)/user/nfqws/nfqws /usr/bin/nfqws
+EOF
